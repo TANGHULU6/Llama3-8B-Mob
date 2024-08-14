c29febc8a85ce20edaa39210a04bcd818bb6c3b2
TANGHULU6 <12110502@mail.sustech.edu.cn>
2024-08-14 18:19:43 +0900

    4load

--- Finetune_Llama3.py
+++ Finetune_Llama3.py
@@ -129,7 +129,7 @@ tokenizer = get_chat_template(
 
 # Load and format the custom dataset
 train_dataset = load_custom_dataset("dataset60000_not_completed.json")
-train_dataset = train_dataset.select(range(15000))
+train_dataset = train_dataset.select(range(10000))
 train_dataset = train_dataset.map(formatting_prompts_func, batched=True)
 test_dataset = load_custom_dataset("dataset60000-79999_not_completed.json")
 test_dataset = test_dataset.select(range(100))
@@ -289,15 +289,15 @@ To save the final model as LoRA adapters, either use Huggingface's `push_to_hub`
 **[NOTE]** This ONLY saves the LoRA adapters, and not the full model. To save to 16bit or GGUF, scroll down!
 """
 
-model.save_pretrained("lora_model_0814_not_completed_15000") # Local saving
+model.save_pretrained("lora_model_0814_not_completed_10000_4load") # Local saving
 # model.push_to_hub("your_name/lora_model", token = "...") # Online saving
 
 """Now if you want to load the LoRA adapters we just saved for inference, set `False` to `True`:"""
 
-if False:
+if True:
     from unsloth import FastLanguageModel
     model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "lora_model", # YOUR MODEL YOU USED FOR TRAINING
+        model_name = "lora_model_0814_not_completed_10000_4load", # YOUR MODEL YOU USED FOR TRAINING
         max_seq_length = max_seq_length,
         dtype = dtype,
         load_in_4bit = load_in_4bit,
