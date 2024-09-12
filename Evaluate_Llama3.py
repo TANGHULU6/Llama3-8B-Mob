bd239e3dad84c3e6da4599c651e8947a3a453fc3
TANGHULU6 <12110502@mail.sustech.edu.cn>
2024-09-13 03:26:51 +0900

    0913

--- Evaluate_Llama3.py
+++ Evaluate_Llama3.py
@@ -13,7 +13,7 @@ dtype = None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 fo
 load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name = "artifacts/model-First:v35", # YOUR MODEL YOU USED FOR TRAINING
+    model_name = "artifacts/model-B_eval:v55", # YOUR MODEL YOU USED FOR TRAINING
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit,
